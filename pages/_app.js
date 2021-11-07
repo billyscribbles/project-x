a5978749519124de384a27c97c343ac0daf666c5
Billy Huynh <billyhuynh@live.com>
2021-11-07 18:50:26 +1100

    intergrated amplify backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Amplify from 'aws-amplify';
+import config from '../src/aws-exports';
+import '../styles/globals.css';
+
+Amplify.configure({
+  ...config,
+  ssr: true
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
